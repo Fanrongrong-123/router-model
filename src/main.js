f006636dce533451c43bc5fa7802adcbf8b33c09
Fanrr <2514509399@qq.com>
2021-01-06 21:41:36 +0800

    router hash

--- src/main.js
+++ src/main.js
@@ -1,9 +1,25 @@
+const div1 = document.createElement('div')
+div1.innerHTML = '1'
+const div2 = document.createElement('div')
+div2.innerHTML = '2'
+const div3 = document.createElement('div')
+div3.innerHTML = '3'
+const div4 = document.createElement('div')
+div4.innerHTML = '4'
+
+const routerTable = {
+    '1': div1,
+    '2': div2,
+    '3': div3,
+    '4': div4,
+}
+
 function router() {
     let number = window.location.hash.substr(1)
 
     number = number || 1
 
-    let div = document.querySelector(`#div${number}`)
+    let div = routerTable[number.toString()]
 
     if (!div) {
         div = document.querySelector("#div404")
@@ -12,13 +28,13 @@ function router() {
 
     const app = document.querySelector("#app")
 
+    app.innerHTML = ''
+
     app.appendChild(div)
 
 }
 router()
 
 window.addEventListener('hashchange', () => {
-    app.children[0].style.display = "none"
-    document.body.appendChild(app.children[0])
     router()
 })
