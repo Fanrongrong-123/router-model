9c02978aab92da5078791108e5b13c0d6ba5d58a
Fanrr <2514509399@qq.com>
2021-01-06 17:13:57 +0800

    router

--- src/main.js
+++ src/main.js
@@ -1,26 +1,24 @@
-let number = window.location.hash.substr(1)
-
-number = number || 1
-
-const div = document.querySelector(`#div${number}`)
-
-const app = document.querySelector("#app")
-
-div.style.display = "block";
-
-app.appendChild(div)
-
-window.addEventListener('hashchange', () => {
+function router() {
     let number = window.location.hash.substr(1)
 
     number = number || 1
 
-    const div = document.querySelector(`#div${number}`)
-    const app = document.querySelector("#app")
+    let div = document.querySelector(`#div${number}`)
+
+    if (!div) {
+        div = document.querySelector("#div404")
+    }
     div.style.display = "block";
 
-    app.children[0].style.display = "none"
-    document.body.appendChild(app.children[0])
+    const app = document.querySelector("#app")
 
     app.appendChild(div)
+
+}
+router()
+
+window.addEventListener('hashchange', () => {
+    app.children[0].style.display = "none"
+    document.body.appendChild(app.children[0])
+    router()
 })
